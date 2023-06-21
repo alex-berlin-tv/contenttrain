ce4e867d13c10294966073424939684be5364213
alex-berlin-tv <hostettler@alex-berlin.de>
2023-06-21 12:04:39 +0200

    only execute update actions on server if needed

--- train/update.py
+++ train/update.py
@@ -14,16 +14,24 @@ def do_update(episodes: NocoEpisodes):
 def handle_item(episodes: NocoEpisodes, episode: NocoEpisode, count: int, total: int):
     progress = f"[{count}/{total}]"
     description = f"item e-{episode.noco_id}, title: '{episode.title}'"
-    if not episode.server_index or episode.server_index in ["X", ""]:
+    if not episode.server_index or \
+       episode.server_index in ["X", ""] and \
+       episode.source_state != SourceState.unknown:
         print(f"{progress} Set SourceState to Unknown {description}")
         episodes.update_source_state(episode, SourceState.unknown)
-    if episode.server_index and episode.server_index.startswith("\\\\"):
+    if episode.server_index and \
+       episode.server_index.startswith("\\\\") and \
+       episode.source_state != SourceState.disa_server:
         print(f"{progress} Set SourceState to DiSa-Server {description}")
         episodes.update_source_state(episode, SourceState.disa_server)
-    if not episode.server_index and episode.youtube_url:
+    if not episode.server_index and \
+       episode.youtube_url and \
+       episode.source_state != SourceState.youtube:
         print(f"{progress} Set SourceState to YouTube {description}")
         episodes.update_source_state(episode, SourceState.youtube)
     if episode.source_state == SourceState.disa_server and episode.server_index:
         print(f"{progress} Set Source File from Server Index {description}")
         suffix = Path(episode.server_index).suffix
-        episodes.update_source_file(episode, str(Path(episode.file_name()).with_suffix(suffix)))
+        file_path = str(Path(episode.file_name()).with_suffix(suffix))
+        if episode.source_file != file_path:
+            episodes.update_source_file(episode, file_path)
