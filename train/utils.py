b31b2bba4b648973c8837811e7fdb7f560bb1e08
alex-berlin-tv <hostettler@alex-berlin.de>
2023-06-21 13:47:26 +0200

    ignore files

--- train/utils.py
+++ train/utils.py
@@ -4,18 +4,12 @@ import glob
 import os
 import re
 
-IGNORE = ["desktop.ini"]
-
-
 def transcoded_items_present() -> dict[int, str]:
     rsl: dict[int, str] = {}
     files: list[str] = glob.glob(os.path.join(settings.transcoding_destination_folder, "*")) # type: ignore
     pattern = re.compile(r"e-(\d+)_.*")
     for file in files:
-        if file in IGNORE:
-            continue
         id = pattern.match(file)
-        if not id:
-            raise ValueError(f"couldn't parse id from filename {file}")
-        rsl[int(id.group(1))] = file
+        if id:
+            rsl[int(id.group(1))] = file
     return rsl
