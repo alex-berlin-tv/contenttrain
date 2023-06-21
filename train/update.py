9f82ce6c026359cc33fb17891b65268dd2dd0a1c
alex-berlin-tv <hostettler@alex-berlin.de>
2023-06-21 12:10:09 +0200

    fixed update soure_file progress bug

--- train/update.py
+++ train/update.py
@@ -16,22 +16,22 @@ def handle_item(episodes: NocoEpisodes, episode: NocoEpisode, count: int, total:
     description = f"item e-{episode.noco_id}, title: '{episode.title}'"
     if not episode.server_index or \
        episode.server_index in ["X", ""] and \
-       episode.source_state != SourceState.unknown:
+       episode.source_state != SourceState.UNKNOWN:
         print(f"{progress} Set SourceState to Unknown {description}")
-        episodes.update_source_state(episode, SourceState.unknown)
+        episodes.update_source_state(episode, SourceState.UNKNOWN)
     if episode.server_index and \
        episode.server_index.startswith("\\\\") and \
-       episode.source_state != SourceState.disa_server:
+       episode.source_state != SourceState.DISA_SERVER:
         print(f"{progress} Set SourceState to DiSa-Server {description}")
-        episodes.update_source_state(episode, SourceState.disa_server)
+        episodes.update_source_state(episode, SourceState.DISA_SERVER)
     if not episode.server_index and \
        episode.youtube_url and \
-       episode.source_state != SourceState.youtube:
+       episode.source_state != SourceState.YOUTUBE:
         print(f"{progress} Set SourceState to YouTube {description}")
-        episodes.update_source_state(episode, SourceState.youtube)
-    if episode.source_state == SourceState.disa_server and episode.server_index:
-        print(f"{progress} Set Source File from Server Index {description}")
+        episodes.update_source_state(episode, SourceState.YOUTUBE)
+    if episode.source_state == SourceState.DISA_SERVER and episode.server_index:
         suffix = Path(episode.server_index).suffix
         file_path = str(Path(episode.file_name()).with_suffix(suffix))
         if episode.source_file != file_path:
+            print(f"{progress} Set Source File from Server Index {description}")
             episodes.update_source_file(episode, file_path)
