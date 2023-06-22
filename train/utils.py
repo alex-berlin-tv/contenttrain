8e2b17cfa5c7d36ded0a759fa1eea1f1e43a30f7
alex-berlin-tv <hostettler@alex-berlin.de>
2023-06-22 11:46:36 +0200

    debug output in utils.py

--- train/utils.py
+++ train/utils.py
@@ -9,6 +9,7 @@ def transcoded_items_present() -> dict[int, str]:
     rsl: dict[int, str] = {}
     folder = Path(settings.transcoding_destination_folder) # type: ignore
     files = [file for file in folder.iterdir() if file.is_file()]
+    print(files)
     pattern = re.compile(r"e-(\d+)_.*")
     for file in files:
         id = pattern.match(str(file))
