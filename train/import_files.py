296168e9f73221f4ba2a7e0d4f223f4802f39bef
alex-berlin-tv <hostettler@alex-berlin.de>
2023-06-22 18:31:36 +0200

    skip already transcoded files during import

--- train/import_files.py
+++ train/import_files.py
@@ -15,7 +15,9 @@ def do_import_files(episodes: NocoEpisodes):
 def handle_item(episodes: NocoEpisodes, episode: NocoEpisode, count: int, total: int) -> int:
     progress = f"[{count}/{total}]"
     description = f"item e-{episode.noco_id}, title: '{episode.title}'"
-    if episode.source_state != SourceState.DISA_SERVER:
+    if episode.source_state != SourceState.DISA_SERVER or \
+       episode.file_on_edit_state == FileState.DONE or \
+       episode.is_transcoded:
         return count
     if not episode.server_index or episode.server_index == "":
         print(f"No server_index path for item with id e-{episode.noco_id} and title '{episode.title}' ignoring this entry")
@@ -23,7 +25,7 @@ def handle_item(episodes: NocoEpisodes, episode: NocoEpisode, count: int, total:
     if episode.file_on_edit_state == FileState.EXISTS:
         print(f"{progress} Already copied {description}")
         return count + 1
-    print(f"{progress} Copy from server to temp {description}")
+    print(f"{progress} Copy from server {description}")
     source = episode.local_server_source_path()
     destination = Path(settings.transcoding_source_folder) / Path(episode.file_name()).with_suffix(source.suffix) # type: ignore
     if not source:
